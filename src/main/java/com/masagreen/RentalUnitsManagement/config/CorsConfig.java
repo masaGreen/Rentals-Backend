00f26c600453f330e8fbda7d3f73aa0b14aad90c
masaGreen <davidmachariamj@gmail.com>
2023-11-27 00:14:02 +0300

    cors config

--- src/main/java/com/masagreen/RentalUnitsManagement/config/CorsConfig.java
+++ src/main/java/com/masagreen/RentalUnitsManagement/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173")
+        .allowedOrigins("http://localhost:5173", "https://myrentalmanager.onrender.com")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("Origin", "Authorization", "Content-Type", "Accept", "responseType")
         .allowCredentials(true)
